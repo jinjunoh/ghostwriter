aa46c180cd280fdae402b0c4296cca3ac13eef97
JJ <jinjunoh2@gmail.com>
2023-05-04 00:27:18 -0500

    getComm function which checks finish_reason from data

--- api/index.js
+++ api/index.js
@@ -9,17 +9,34 @@ const openai = new OpenAiApi(config);
 
 const app = express();
 app.use(bodyParser.json());
+let messages = [];
+
 app.post('/write', (req, res) => {
     const{message, style} = req.body; // message is the input from the user, style is how the system will answer
-    openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
+    messages.push([
             {role: "system", content: style},
             {role: 'user', content: message}
-        ]
+    ])
+    getComm(res);
+})
+
+function gptComm(res){
+    openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages
     }).then((data) => {
-        res.send(data.data.choices[0].message.content);
+        messages.push(
+            {role: "assistant", content: data.data.choices[0].message.content}
+        );
+        if(data.data.choices[0].finish_reason !== "stop"){
+            message.push(
+                {role: "user", content: 'continue'}
+            );
+            getSelectionRange(res);
+        } else{
+            res.send(messages.filter((message) => message.role === "assistant").map((message)=> message.content));
+        }
     }).catch((err)=> {
         console.log(err);
-    })
-})
+    });
+}
